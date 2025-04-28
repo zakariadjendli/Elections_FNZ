90d74d3ab59a874f10168fa3f2d9206620ed5a45
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-04-28 14:14:34 -0400

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,9 @@
 fichier = open ("fichier.txt", "r")
 lignes = fichier.readlines()
 print(lignes)
-candidat1 = Zakaria
-candidat2 = Félix
-candidat3 = Nathaniel  
+candidat1 = Zakaria Djendli
+candidat2 = Félix Boisvert
+candidat3 = Nathaniel  Samson
 if candidat1 > candidat2 and candidat3:
     print("Candidat 1 a gagné")
 elif candidat2 > candidat1 and candidat3:
