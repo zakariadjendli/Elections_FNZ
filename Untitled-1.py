9cc6193926abb814f453c3d8d5799a9cb0f75f73
zakariadjendli <202157206@cegepsherbrooke.qc.ca>
2025-04-28 14:08:31 -0400

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+fichier = open ("fichier.txt", "r")
+lignes = fichier.readlines()
+print(lignes)
+candidat1 = Zakaria
+candidat2 = Félix
+candidat3 = Nathaniel  
+if candidat1 > candidat2 and candidat3:
+    print("Candidat 1 a gagné")
+elif candidat2 > candidat1 and candidat3:
+    print("Candidat 2 a gagné")
+elif candidat3 > candidat1 and candidat2:
+    print("Candidat 3 a gagné")
+else:
+    print("Il y a égalité")
+fichier.close()
